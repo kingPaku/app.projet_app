d30fb8bae1c80fea31fc6bd63ae0d7296425640e
Enigmapaku <enigmapaku@gmail.com>
2024-01-24 16:57:45 +0100

    "Suppression "

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:projet_app/view/public/Auth.dart";
 
-void main() => runApp(MyWidget());
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+void main() => runApp(Myapp());
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Shoolsearch',
+      debugShowCheckedModeBanner: false,
+      home : Authentification(),
+    );
   }
 }
